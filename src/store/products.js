0560140ab98e5ac50146eb2ca91e3a4af26f6d74
Lewis Barnes <lb.barnes@live.co.uk>
2016-03-01 22:06:03 +0000

    Added links for each product on the home screen. - Added a zoom effect on hover.

--- src/store/products.js
+++ src/store/products.js
@@ -1,41 +1,49 @@
 export default function() {
     return [
         {
+            id: 1,
             name: "Strawberries",
             imageURL: require("../assets/strawberries.jpg"),
             price: 2.75
         },
         {
+            id: 2,
             name: "Raspberries",
             imageURL: require("../assets/raspberries.jpg"),
             price: 3.49
         },
         {
+            id: 3,
             name: "Watermelon",
             imageURL: require("../assets/watermelons.jpg"),
             price: 2.49
         },
         {
+            id: 4,
             name: "Peaches",
             imageURL: require("../assets/peaches.jpg"),
             price: 1.99
         },
         {
+            id: 5,
             name: "Oranges",
             imageURL: require("../assets/oranges.jpg"),
             price: 1.99
         },
         {
+            id: 6,
             name: "Bananas",
             imageURL: require("../assets/bananas.jpg"),
             price: 1.24
         },
         {
+            id: 7,
             name: "Kiwi",
             imageURL: require("../assets/kiwi.jpg"),
             price: 0.99
         },
         {
+            id: 8,
             name: "Lemons",
             imageURL: require("../assets/lemons.jpg"),
             price: 0.99
