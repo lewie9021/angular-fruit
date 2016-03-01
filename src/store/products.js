e24dab1e7aa9f6e42db51d505821c51a802cd2ee
Lewis Barnes <lb.barnes@live.co.uk>
2016-03-01 21:24:11 +0000

    Moved the static products file to /store. - Added more fruit to the list.

---
+++ src/store/products.js
@@ -0,0 +1,44 @@
+export default function() {
+    return [
+        {
+            name: "Strawberries",
+            imageURL: require("../assets/strawberries.jpg"),
+            price: 2.75
+        },
+        {
+            name: "Raspberries",
+            imageURL: require("../assets/raspberries.jpg"),
+            price: 3.49
+        },
+        {
+            name: "Watermelon",
+            imageURL: require("../assets/watermelons.jpg"),
+            price: 2.49
+        },
+        {
+            name: "Peaches",
+            imageURL: require("../assets/peaches.jpg"),
+            price: 1.99
+        },
+        {
+            name: "Oranges",
+            imageURL: require("../assets/oranges.jpg"),
+            price: 1.99
+        },
+        {
+            name: "Bananas",
+            imageURL: require("../assets/bananas.jpg"),
+            price: 1.24
+        },
+        {
+            name: "Kiwi",
+            imageURL: require("../assets/kiwi.jpg"),
+            price: 0.99
+        },
+        {
+            name: "Lemons",
+            imageURL: require("../assets/lemons.jpg"),
+            price: 0.99
+        }
+    ];
+};
