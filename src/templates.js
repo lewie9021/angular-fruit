569150e59028d327ed4d94550e2596dbd53af3fe
Lewis Barnes <lb.barnes@live.co.uk>
2016-03-06 22:12:48 +0000

    Removed the old templates file.

--- src/templates.js
+++
@@ -1,7 +0,0 @@
-import Home from "./screens/home/view.html";
-import Basket from "./screens/basket/view.html";
-import Product from "./screens/product/view.html";
-
-exports.home = Home;
-exports.basket = Basket;
-exports.product = Product;
