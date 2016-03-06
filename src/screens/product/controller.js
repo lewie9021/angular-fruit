7d4544bf734b4ae7ddf38c05b6968944a3a82f4e
Lewis Barnes <lb.barnes@live.co.uk>
2016-03-06 15:58:47 +0000

    Converted the price property to return a value with 2 deciaml places. - This covers cases where products could cost £2 -> £2.00.

--- src/screens/product/controller.js
+++ src/screens/product/controller.js
@@ -2,7 +2,9 @@ export default function($scope, $state, ProductsModel, BasketModel) {
     const productID = parseInt($state.params.id, 10);
     const [product] = ProductsModel.filter(({id}) => id == productID);
 
-    Object.assign($scope, product);
+    Object.assign($scope, product, {
+        price: product.price.toFixed(2)
+    });
 
     $scope.addToBasket = () => {
         BasketModel.add(productID);
