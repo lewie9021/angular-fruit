7c3c12e8e6b957b6040c42e5ea3d9f07ef9bba32
Lewis Barnes <lb.barnes@live.co.uk>
2016-03-06 20:35:08 +0000

    Added the ability to increment and decrement quantity.

--- src/screens/basket/controller.js
+++ src/screens/basket/controller.js
@@ -2,23 +2,28 @@ export default function($scope, ProductsModel, BasketModel) {
     const products = ProductsModel.reduce((map, product) => {
         return Object.assign(map, {[product.id]: product});
     }, {});
-    const counts = BasketModel.counts();
-    const productIDs = Object.keys(counts);
-    const total = productIDs.reduce((total, productID) => {
-        const product = products[productID];
-        const quantity = counts[productID];
+    const total = () => {
+        const counts = BasketModel.counts();
 
-        return total + (product.price * quantity);
-    }, 0);
+        return Object.keys(counts).reduce((total, productID) => {
+            const product = products[productID];
+            const quantity = counts[productID];
 
-    $scope.products = productIDs.map((productID) => {
-        const product = products[productID];
+            return total + (product.price * quantity);
+        }, 0).toFixed(2);
+    };
 
-        return Object.assign({}, product, {
-            price: product.price.toFixed(2),
-            quantity: counts[productID]
+    $scope.products = Object.keys(BasketModel.counts())
+        .map((productID) => {
+            const product = products[productID];
+
+            return Object.assign({}, product, {
+                price: product.price.toFixed(2),
+                quantity: () => BasketModel.get(productID)
+            });
         });
-    });
 
-    $scope.total = total.toFixed(2);
+    $scope.total = total;
+    $scope.add = (productID) => BasketModel.add(productID);
+    $scope.remove = (productID) =>  BasketModel.remove(productID);
 }
