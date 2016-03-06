937701ae934a31630796a5700c6f3e5a70043c16
Lewis Barnes <lb.barnes@live.co.uk>
2016-03-06 12:48:28 +0000

    Simplified the entry file, moving routing onto the screen.

--- src/app.entry.js
+++ src/app.entry.js
@@ -1,38 +1,24 @@
 import Angular from "angular";
 import UIRouter from "angular-ui-router";
 import UIBootstrap from "angular-ui-bootstrap";
-import Templates from "./templates";
 import Home from "./screens/home";
-import Basket from "./screens/basket";
 import Product from "./screens/product";
+import Basket from "./screens/basket";
 
 import "bootstrap/dist/css/bootstrap.css";
 import "./theme.scss";
 
-Angular.module("main", [UIRouter, UIBootstrap, Home, Basket, Product])
-    .config(($stateProvider, $urlRouterProvider) => {
-        $urlRouterProvider.otherwise("/");
+const dependencies = [
+    UIRouter,
+    UIBootstrap,
+    Home,
+    Product,
+    Basket
+];
 
-        $stateProvider
-            .state("home", {
-                url: "/",
-                template: Templates.home,
-                controller: "HomeCtrl"
-            })
-            .state("basket", {
-                url: "/basket",
-                template: Templates.basket,
-                controller: "BasketCtrl"
-            })
-            .state("product", {
-                url: "/product/:id",
-                template: Templates.product,
-                controller: "ProductCtrl"
-            });
-    })
-    .controller("MainCtrl", function($scope, BasketModel) {
-        const total = Object.keys(BasketModel)
-                  .reduce((total, key) => total + BasketModel[key], 0);
-
-        $scope.total = total;
+Angular.module("app", dependencies)
+    .config(($urlRouterProvider) => {
+        // TODO: Figure out how to actually get this to work without
+        // declaring all my routes here.
+        // $urlRouterProvider.otherwise("/");
     });
