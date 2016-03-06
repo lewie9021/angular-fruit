f38d924c0abcfb08a28d3a813f3a670004c023ea
Lewis Barnes <lb.barnes@live.co.uk>
2016-03-06 13:39:55 +0000

    Initial stab at the basket API. - This provides a key map of productID to count.

--- src/store/basket.js
+++ src/store/basket.js
@@ -1,4 +1,33 @@
 export default function() {
     // TODO: This could look in local storage on intial load.
-    return {};
+    let basket = {};
+
+    return {
+        counts: () => {
+            return basket;
+        },
+        total: () => {
+            return Object.keys(basket)
+                .reduce((accumulator, key) => accumulator + basket[key], 0);;
+        },
+        add: (productID) => {
+            const count = basket[productID] || 0;
+
+            basket[productID] = count  + 1;
+        },
+        remove: (productID) => {
+            const count = basket[productID];
+
+            if (count)
+                return basket[productID] = count - 1;
+
+            delete basket[productID];
+        },
+        clear: (productID) => {
+            delete basket[productID];
+        },
+        empty: () => {
+            basket = {};
+        }
+    };
 };
