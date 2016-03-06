7ae7714e5e15ddd44b02432b5a4411a7a8c213ee
Lewis Barnes <lb.barnes@live.co.uk>
2016-03-06 15:56:37 +0000

    Fixed a display bug with the order total regarding rounding.

--- src/screens/basket/controller.js
+++ src/screens/basket/controller.js
@@ -1,22 +1,23 @@
 export default function($scope, ProductsModel, BasketModel) {
+    // TODO: Convert the ProductsModel into a lookup object (key'd by ID).
+    const getProduct = (productID) => ProductsModel.filter(({id}) => id == productID)[0];
     const counts = BasketModel.counts();
     const productIDs = Object.keys(counts);
-    const getProduct = (productID) => ProductsModel.filter(({id}) => id == productID)[0];
+    const total = productIDs.reduce((total, productID) => {
+        const product = getProduct(productID);
+        const quantity = counts[productID];
 
-    $scope.products = productIDs
-        .map((productID) => {
-            const product = getProduct(productID);
+        return total + (product.price * quantity);
+    }, 0);
 
-            return Object.assign({}, product, {
-                quantity: counts[productID]
-            });
+    $scope.products = productIDs.map((productID) => {
+        const product = getProduct(productID);
+
+        return Object.assign({}, product, {
+            price: product.price.toFixed(2),
+            quantity: counts[productID]
         });
+    });
 
-    $scope.total = productIDs
-        .reduce((total, productID) => {
-            const product = getProduct(productID);
-            const quantity = counts[productID];
-
-            return total + (product.price * quantity);
-        }, 0);
+    $scope.total = total.toFixed(2);
 }
