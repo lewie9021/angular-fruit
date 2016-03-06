cbc0c504a456d1454088cc1c97b8b59f008fa464
Lewis Barnes <lb.barnes@live.co.uk>
2016-03-06 14:49:09 +0000

    Initial attempt at the basket screen.

--- src/screens/basket/controller.js
+++ src/screens/basket/controller.js
@@ -1,4 +1,12 @@
 export default function($scope, ProductsModel, BasketModel) {
-    console.log("BasketCtrl", ProductsModel, BasketModel.counts());
+    const counts = BasketModel.counts();
 
+    $scope.products = Object.keys(counts)
+        .map((productID) => {
+            const [product] = ProductsModel.filter(({id}) => id == productID);
+
+            return Object.assign({}, product, {
+                quantity: counts[productID]
+            });
+        });
 }
